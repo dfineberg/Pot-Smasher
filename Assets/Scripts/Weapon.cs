aa4ce57680afdfd2a3c062a4398728d3057f21c8
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2016-01-29 21:14:10 +0000

    Added player animator

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+    Animator animator;
+    int downHash;
+    int leftHash;
+    int rightHash;
+    int upHash;
+    int idleHash;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        downHash = Animator.StringToHash("Down");
+        leftHash = Animator.StringToHash("Left");
+        rightHash = Animator.StringToHash("Right");
+        upHash = Animator.StringToHash("Up");
+        idleHash = Animator.StringToHash("Idle");
+    }
+
+    public virtual bool IsAttacking()
+    {
+        if(animator.GetCurrentAnimatorStateInfo(0).shortNameHash != idleHash)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+	public virtual void Attack(Direction attackDir)
+    {
+        switch (attackDir)
+        {
+            case Direction.up:
+                animator.SetTrigger(upHash);
+                break;
+
+            case Direction.right:
+                animator.SetTrigger(rightHash);
+                break;
+
+            case Direction.down:
+                animator.SetTrigger(downHash);
+                break;
+
+            case Direction.left:
+                animator.SetTrigger(leftHash);
+                break;
+        }
+    }
+}
