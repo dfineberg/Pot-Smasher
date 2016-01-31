9765174c86524d4592b5d1831249372e6ec273ab
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2016-01-31 12:58:02 +0000

    SMG now fires bullets

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour {
+
+    public float speed;
+
+    int damage;
+    Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    public void SetDamage(int d)
+    {
+        damage = d;
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody.MovePosition(rigidbody.position + (Vector2)(transform.TransformDirection(Vector3.up) * speed * Time.fixedDeltaTime));
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Pot"))
+        {
+            col.GetComponent<PS.Pot>().Damage(damage);
+        }
+
+        if (!col.CompareTag("Player") && !col.GetComponent<Bullet>())
+        {
+            Destroy(gameObject);
+        }
+    }
+}
