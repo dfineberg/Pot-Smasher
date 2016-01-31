78c907c7709d3be59f20f73a7b47112889ed838e
Zion Siton <zionsiton1@gmail.com>
2016-01-31 13:11:39 +0000

    Added PlayerCamera.Instance.Shake()

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -3,17 +3,37 @@ using System.Collections;
 
 public class PlayerCamera : MonoBehaviour {
 
+	public static PlayerCamera Instance{private set;get;}
+
+	void Awake () 
+	{
+		if ( Instance ) DestroyImmediate( this.gameObject );
+		else Instance = this;
+	}
+
 	PlayerController player;
+	Vector2 offSet;
+	public float shakeForce = 5.0f;
+	public float returnSpeed = 10.0f;
+	public float maxOffset = 2.0f;
 
 	// Use this for initialization
-	void Start () {
-	
+	public void Shake () 
+	{
+		Vector2 nudge = new Vector2((Random.value * 2 - 1) * shakeForce, (Random.value * 2 - 1) * shakeForce);
+		offSet += nudge;
+
+		if (offSet.magnitude > maxOffset) offSet = offSet.normalized * maxOffset;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
+		if (Input.GetKeyDown(KeyCode.P)) Shake();
+
 		if (player == null) player = PlayerController.instance;
-		transform.position = Vector3.Lerp(transform.position, player.transform.position + new Vector3(0,0,-10), Time.deltaTime);
+		transform.position = Vector3.Lerp(transform.position, player.transform.position + new Vector3(0,0,-10) + (Vector3)offSet, Time.deltaTime);
+
+		offSet = Vector2.Lerp( offSet, Vector2.zero, Time.deltaTime * returnSpeed );
 	}
 }
--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -1,15 +1,85 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class SoundController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+namespace PS
+{
+	public enum SOUND_ID
+	{
+		NONE,
+		POT_SHAKE
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	class PLAY_SOUND
+	{
+		public SOUND_ID ID;
+		public float VOLUME;
+
+		public PLAY_SOUND(SOUND_ID id = SOUND_ID.NONE, float volume = 0.0f)
+		{
+			ID = id; VOLUME = volume;
+		}
+	}
+
+	public class SoundController : MonoBehaviour 
+	{
+		Dictionary<SOUND_ID,AudioSource> soundList;
+
+		const int MAX_PENDING = 10;
+		int HEAD;
+		int TAIL;
+		PLAY_SOUND[] soundQueue;
+
+		public static SoundController Instance{private set;get;}
+
+		void Awake () 
+		{
+			if ( Instance ) DestroyImmediate( this.gameObject );
+			else Instance = this;
+
+			DontDestroyOnLoad(this);
+			soundList = new Dictionary<SOUND_ID, AudioSource>();
+			soundQueue = new PLAY_SOUND[MAX_PENDING];
+			HEAD = TAIL = 0;
+		}
+
+		// Use this for initialization
+		public void PlaySound (SOUND_ID id, float volume = 0.5f) 
+		{
+			volume = Mathf.Clamp01(volume);
+
+			// Walk the pending requests.
+			for (int i = HEAD; i != TAIL; i = (i + 1) % MAX_PENDING)
+			{
+				if (soundQueue[i].ID == id)
+				{
+					// Use the larger of the two volumes.
+					soundQueue[i].VOLUME = Mathf.Max(volume, soundQueue[i].VOLUME);
+
+					// Don't need to enqueue.
+					return;
+				}
+			}
+				
+			if((TAIL + 1) % MAX_PENDING != HEAD)
+			{
+				// Add to the end of the list.
+				soundQueue[TAIL].ID = id;
+				soundQueue[TAIL].VOLUME = volume;
+				TAIL = (TAIL + 1) % MAX_PENDING;
+			}
+		}
+		
+		// Update is called once per frame
+		void LateUpdate () 
+		{
+			// If there are no pending requests, do nothing.
+			if (HEAD == TAIL) return;
+
+			AudioSource sound = soundList[soundQueue[HEAD].ID];
+			sound.volume = soundQueue[HEAD].VOLUME;
+			sound.Play();
+			HEAD = (HEAD + 1) % MAX_PENDING;
+		}
 	}
 }
